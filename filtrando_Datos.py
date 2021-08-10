486cfa65d78667017072a2103687843a26daebbf
Victor Huatuco <victorhuatuco@gmail.com>
2021-08-09 23:37:43 -0500

    Mejora

--- filtrando_Datos.py
+++ filtrando_Datos.py
@@ -74,11 +74,24 @@ DATA = [
 
 def run():
     all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+
+    all_python_devs_list = list(filter(lambda worker: worker["language"] == "python", DATA))
+    all_python_devs_list = list(map(lambda worker: worker["name"], all_python_devs_list))
+
     all_python_workers = [worker["name"] for worker in DATA if worker ["organization"] == "Platzi"]
+
+    all_python_workers_list = list(filter(lambda worker: worker["organization"] =="Platzi", DATA))
+    all_python_workers_list = list(map(lambda worker: worker["name"], all_python_workers_list))
+    
     adults = list(filter(lambda worker: worker["age"] > 18, DATA))
     adults = list(map(lambda worker: worker["name"], adults))
+    adults_comprehensions = [worker["name"] for worker in DATA if worker["age"] >18]
+
+
     old_people =  list(map(lambda worker: worker | {"old": worker["age"] < 70}, DATA))
-    for worker in all_python_workers:
+    # old_people_list = [worker | {"old":worker["age"] < 70},
+
+    for worker in adults_comprehensions:
         print(worker)
 
 if __name__ == '__main__':
